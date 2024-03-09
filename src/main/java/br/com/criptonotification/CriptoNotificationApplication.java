48718fe3a011aaa74af91a2e3c4782ef77e3193d
rayan.garcia <rayan.garcia@efabrika.com.br>
2024-03-09 12:10:49 -0300

    iniciando projeto

---
+++ src/main/java/br/com/criptonotification/CriptoNotificationApplication.java
@@ -0,0 +1,13 @@
+package br.com.criptonotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriptoNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/criptonotification/CriptoNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.criptonotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
